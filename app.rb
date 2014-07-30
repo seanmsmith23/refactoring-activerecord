6556ea4e052072e3b344c6953285b417c3f9c572
Sean Smith <seansmith1020@gmail.com>
2014-07-30 14:47:26 -0600

    removed initialize that contained db_connection

--- app.rb
+++ app.rb
@@ -8,11 +8,6 @@ class App < Sinatra::Application
   enable :sessions
   use Rack::Flash
 
-  def initialize
-    super
-    @database_connection = GschoolDatabaseConnection::DatabaseConnection.establish(ENV["RACK_ENV"])
-  end
-
   get "/" do
     user = current_user
 
